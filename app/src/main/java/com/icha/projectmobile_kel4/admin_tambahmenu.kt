e3fd4d9a91cedecf2d439c793c555e49cd9b1047
farahdilla <farahdilla18ti@mahasiswa.pcr.ac.id>
2021-01-15 23:07:38 +0700

    commit 15/1/2021 farah_branch Halaman Fragment, admin tambah menu, Halaman Menu, Layout admin_tambahmenu, fragment makanan, minuman, cemilan, halaman_menu, login, register

--- app/src/main/java/com/icha/projectmobile_kel4/admin_tambahmenu.kt
+++ app/src/main/java/com/icha/projectmobile_kel4/admin_tambahmenu.kt
@@ -8,11 +8,9 @@ import android.widget.*
 import kotlinx.android.synthetic.main.admin_tambahmenu.*
 
 class admin_tambahmenu : AppCompatActivity(),AdapterView.OnItemSelectedListener {
-
     var categori = arrayOf("Makanan", "Minuman", "Cemilan")
     var spinner:Spinner? = null
     var textView_msg:TextView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.admin_tambahmenu)
@@ -28,12 +26,9 @@ class admin_tambahmenu : AppCompatActivity(),AdapterView.OnItemSelectedListener 
         spinner!!.setAdapter(aa)
 
     }
-
     override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
         textView_msg!!.text = "Selected : "+categori[position]
     }
-
     override fun onNothingSelected(arg0: AdapterView<*>) {
-
     }
 }
