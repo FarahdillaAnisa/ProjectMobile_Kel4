e4a9c1cc30ba02972bf9f7cc7013c620d5dbd247
abdulArif <52515011+Abdul18TI@users.noreply.github.com>
2021-01-18 21:46:31 +0700

    ProgressRegister

---
+++ app/src/main/java/com/icha/projectmobile_kel4/menufragment/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.icha.projectmobile_kel4.menufragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
