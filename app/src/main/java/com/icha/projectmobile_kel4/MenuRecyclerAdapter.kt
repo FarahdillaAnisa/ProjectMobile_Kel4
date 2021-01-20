6e6f5337b6f20c22be083699ed6e36387f5f3fcc
farahdilla <farahdilla18ti@mahasiswa.pcr.ac.id>
2021-01-21 00:01:42 +0700

    pengubahan tampilan menu , 20.1

---
+++ app/src/main/java/com/icha/projectmobile_kel4/MenuRecyclerAdapter.kt
@@ -0,0 +1,17 @@
+package com.icha.projectmobile_kel4
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.fragment_makanan.*
+import kotlinx.android.synthetic.main.fragment_makanan.view.*
+
