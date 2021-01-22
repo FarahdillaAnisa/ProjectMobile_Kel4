2432963623f1b96ed5bcbc16dd39872584a55957
farahdilla <farahdilla18ti@mahasiswa.pcr.ac.id>
2021-01-23 06:48:59 +0700

    tampilan kasir
    navigationdrawer

--- app/src/main/java/com/icha/projectmobile_kel4/MenuKasir.kt
+++ app/src/main/java/com/icha/projectmobile_kel4/MenuKasir.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
@@ -35,7 +36,7 @@ class MenuKasir  : AppCompatActivity(),NavigationView.OnNavigationItemSelectedLi
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
             R.id.home -> {
-                berandaKasirFragment = BerandaKasirFragment()
+                berandaKasirFragment = berandaKasirFragment
                 supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.frame_layout, berandaKasirFragment)
@@ -65,6 +66,10 @@ class MenuKasir  : AppCompatActivity(),NavigationView.OnNavigationItemSelectedLi
                     .commit()
                 Toast.makeText(applicationContext, "List Reservation", Toast.LENGTH_SHORT)
                     .show()
+            }
+
+            R.id.logoutmenu -> {
+                setContentView(R.layout.login)
             }
         }
         drawerLayout.closeDrawers()
--- app/src/main/java/com/icha/projectmobile_kel4/kasirfragment/ListMenuKasirFragment.kt
+++ app/src/main/java/com/icha/projectmobile_kel4/kasirfragment/ListMenuKasirFragment.kt
@@ -5,14 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.Toolbar
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
-import com.icha.projectmobile_kel4.HalamanMenu
 import com.icha.projectmobile_kel4.R
-import com.icha.projectmobile_kel4.menufragment.CemilanFragment
-import com.icha.projectmobile_kel4.menufragment.MakananFragment
-import com.icha.projectmobile_kel4.menufragment.MinumanFragment
 
 class ListMenuKasirFragment : Fragment() {
     override fun onCreateView(
@@ -21,7 +14,6 @@ class ListMenuKasirFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list_menu_kasir, container, false)
+
     }
-
-
 }
