fe1ce7f227edb97e8086713334f67f889694462f
abdulArif <52515011+Abdul18TI@users.noreply.github.com>
2021-01-25 16:53:10 +0700

    Update tambah Menu

--- app/src/main/java/com/icha/projectmobile_kel4/HomeAdminActivity.kt
+++ app/src/main/java/com/icha/projectmobile_kel4/HomeAdminActivity.kt
@@ -1,11 +1,21 @@
 package com.icha.projectmobile_kel4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_home_admin.*
 
 class HomeAdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_admin)
+
+        val btnmenu = findViewById<ImageView>(R.id.btnMenu)
+        btnmenu.setOnClickListener{
+            val intent1 = Intent(this, admin_tambahmenu::class.java)
+            startActivity(intent1)
+        }
     }
 }
